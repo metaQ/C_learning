b7cdf2e43ed6ff4cca45d537be9ab6d9ab04f61f
metaQ <qiutianchen@126.com>
2013-03-20 01:47:05 -0700

    just can run

--- deepth_maze/method_1/deepth_maze1.c
+++ deepth_maze/method_1/deepth_maze1.c
@@ -8,6 +8,7 @@ int maze[5][5] = {
 	0, 0, 0, 1, 0
 };
 
+
 struct point {
 	unsigned char l;
 	unsigned char c;
@@ -18,53 +19,55 @@ struct element {
 	struct point previous;
 };
 
-void printmaze(struct element a)
+struct element a = {0, 0, 0, 0};
+
+void printmaze(struct element q)
 {
 	int i, j;
 	for(i = 0; i < 5; i++)
 	{
 		for(j = 0; j < 5; j++)
 		{
-			if(a.now.l == i && a.now.c ==j)
+			if(q.now.l == i && q.now.c == j)
 				printf("Q ");
 			else 
 				printf("%d ", maze[i][j]);
 		}
 		printf("\n");
 	}
-	printf("**********\n");
+	printf("*********\n");
 }
 
-unsigned char forward(struct element a)
+unsigned char forward(void)
 {
 	switch(1)
 	{
 		case 1://1:down
-			if((a.now.c+1 != 4) && (maze[a.now.c+1][a.now.l] != 1) && (a.now.c+1 != a.previous.c))
-			{
-				a.previous = a.now;
-				a.now.c++;
-				return 1;
-			}
-		case 2://2:right
-			if((a.now.l+1 != 4) && (maze[a.now.c][a.now.l+1] != 1) && (a.now.l+1 != a.previous.l))
+			if((a.now.l+1 != 5) && (maze[a.now.l+1][a.now.c] != 1) && (a.now.l+1 != a.previous.l))
 			{
 				a.previous = a.now;
 				a.now.l++;
+				return 1;
+			}
+		case 2://2:right
+			if((a.now.c+1 != 5) && (maze[a.now.l][a.now.c+1] != 1) && (a.now.c+1 != a.previous.c))
+			{
+				a.previous = a.now;
+				a.now.c++;
 				return 2;
 			}
 		case 3://3:up
-			if((a.now.c-1 != 0) && (maze[a.now.c-1][a.now.l] != 1) && (a.now.c-1 != a.previous.c))
-			{
-				a.previous = a.now;
-				a.now.c--;
-				return 3;
-			}
-		case 4://4:left
-			if((a.now.l-1 != 0) && (maze[a.now.c][a.now.l-1] != 1) && (a.now.l-1 != a.previous.c))
+			if((a.now.l-1 >= 0) && (maze[a.now.l-1][a.now.c] != 1) && (a.now.l-1 != a.previous.l))
 			{
 				a.previous = a.now;
 				a.now.l--;
+				return 3;
+			}
+		case 4://4:left
+			if((a.now.c-1 >= 0) && (maze[a.now.l][a.now.c-1] != 1) && (a.now.c-1 != a.previous.l))
+			{
+				a.previous = a.now;
+				a.now.c--;
 				return 4;
 			}
 		default:
@@ -72,36 +75,36 @@ unsigned char forward(struct element a)
 	}
 }
 
-void back(struct element a)
+void back(void)
 {
 	switch(1)
 	{
 		case 1://1:up
-			if((a.now.c-1 != 0) && (maze[a.now.c-1][a.now.l] != 1))
-			{
-				a.previous = a.now;
-				a.now.c--;
-				break;
-			}
-		case 2://2:left
-			if((a.now.l-1 != 0) && (maze[a.now.c][a.now.l-1] != 1))
+			if((a.now.l-1 >= 0) && (maze[a.now.l-1][a.now.c] != 1))
 			{
 				a.previous = a.now;
 				a.now.l--;
 				break;
 			}
-		case 3://3:down
-			if((a.now.c+1 != 4) && (maze[a.now.c+1][a.now.l] != 1))
+		case 2://2:left
+			if((a.now.c-1 >= 0) && (maze[a.now.l][a.now.c-1] != 1))
 			{
 				a.previous = a.now;
-				a.now.c++;
+				a.now.c--;
 				break;
 			}
-		case 4://4:right
-			if((a.now.l+1 != 4) && (maze[a.now.c][a.now.l+1] != 1))
+		case 3://3:down
+			if((a.now.l+1 != 5) && (maze[a.now.l+1][a.now.c] != 1))
 			{
 				a.previous = a.now;
 				a.now.l++;
+				break;
+			}
+		case 4://4:right
+			if((a.now.c+1 != 5) && (maze[a.now.l][a.now.c+1] != 1))
+			{
+				a.previous = a.now;
+				a.now.c++;
 				break;
 			}
 		default:
@@ -112,14 +115,13 @@ void back(struct element a)
 int main(void)
 {
 	unsigned char flag = 0;
-	struct element Q = {0, 0, 0, 0};
-	printmaze(Q);
+	printmaze(a);
 	do {
-		flag = forward(Q);
+		flag = forward();
 		if(flag == 0)
-			back(Q);
-		printmaze(Q);
-		} while(Q.now.c + Q.now.l != 0);
+			back();
+		printmaze(a);
+		} while((a.now.c + a.now.l != 0) && (a.now.c + a.now.l != 8));
 	return 0;
 }
 
